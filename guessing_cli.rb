7067aace74e3c0e6251259c3d501c43c686aaf04
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-29 03:42:36 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,7 +4,6 @@ def run_guessing_game
 number = rand 1..6
 puts "Guess a number between 1 and 6."
 guess = gets.chomp  # guess is currently a  string
-binding.pry 
 while guess
 if number == guess.to_i
   puts "You guessed the correct number!"
