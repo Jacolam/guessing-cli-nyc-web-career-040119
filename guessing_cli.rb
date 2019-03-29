6af7ef5fb6526168c11408ca10b3ba0fe78116b7
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-29 03:42:08 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,39 +1,37 @@
 # require 'pry'
 
-# def run_guessing_game
-# number = rand 1..6
-# puts "Guess a number between 1 and 6."
-# guess = gets.chomp  # guess is currently a  string
-# binding.pry 
-# while guess
-# if number == guess.to_i
-#   puts "You guessed the correct number!"
-#   break
-# elsif guess == "exit"
-#       puts "Goodbye!"
-#       break
-# else 
-#   puts "The computer guessed <#{number}>"
-#   break
-# end 
-
-# end 
-
-# end 
-
 def run_guessing_game
-  input = ""
-  while input
-    puts "Guess a number between 1 and 6."
-    input = gets.chomp
-    random = rand(1..6)
-    if input.to_i == random
-      puts "You guessed the correct number!"
-    elsif input == "exit"
+number = rand 1..6
+puts "Guess a number between 1 and 6."
+guess = gets.chomp  # guess is currently a  string
+binding.pry 
+while guess
+if number == guess.to_i
+  puts "You guessed the correct number!"
+  break
+elsif guess == "exit"
       puts "Goodbye!"
       break
-    else
-      puts "The computer guessed #{random}."
-    end
-  end 
-end
+else 
+  puts "The computer guessed <#{number}>"
+  break
+end 
+end 
+end 
+
+# def run_guessing_game
+#   input = ""
+#   while input
+#     puts "Guess a number between 1 and 6."
+#     input = gets.chomp
+#     random = rand(1..6)
+#     if input.to_i == random
+#       puts "You guessed the correct number!"
+#     elsif input == "exit"
+#       puts "Goodbye!"
+#       break
+#     else
+#       puts "The computer guessed #{random}."
+#     end
+#   end 
+# end
