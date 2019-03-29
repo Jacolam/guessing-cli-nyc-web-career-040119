ea5e85d86d30cd3eece57fbffdf1aeea08523464
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-29 03:46:41 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -16,18 +16,4 @@ else
 end 
 end 
 end 
-#   input = ""
-#   while input
-#     puts "Guess a number between 1 and 6."
-#     input = gets.chomp
-#     random = rand(1..6)
-#     if input.to_i == random
-#       puts "You guessed the correct number!"
-#     elsif input == "exit"
-#       puts "Goodbye!"
-#       break
-#     else
-#       puts "The computer guessed #{random}."
-#     end
-#   end 
-# end
+
